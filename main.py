9f558a960809deafc6c7b7b1a0e4322d216cb13e
ValdoSB <oswaldo.sandoval0241@alumnos.udg.mx>
2023-11-05 13:46:42 -0600

    Conexión con Arduino. Temperatura terminada

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import sys
+import serial, time
 from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.uic import loadUi
 
 class MyWindow(QMainWindow):
+    global arduino 
+    arduino = serial.Serial('COM3', 9600)
+
     def __init__(self):
         super(MyWindow, self).__init__()
         loadUi('carniceria.ui', self)
-
+        
         self.timer = QTimer(self) # se crea una variable para constante actualizacion
         self.timer.timeout.connect(lambda:self.actualizarTemperatura())#actualiza el label
-        self.timer.start(1000)#tiempo que tarda en el contador
+        self.timer.start(5000)#tiempo que tarda en el contador
 
     def actualizarTemperatura(self):
-        temperatura = self.lbl_temperatura_actual.text() #ejemplo de agarrar texto de un label
-        temperatura2 = (int(temperatura) + 5) #ejemplo de transformar texto a variable usable 
-        self.lbl_temperatura_actual.setText(str(temperatura2)) #Este es el label que muestra la temperatura actual
+        datosCelsius = arduino.readline()
+        datosFahrenheit = arduino.readline()
+        datosHumedad = arduino.readline()
+
+        if self.comboBox_temperaturas.currentIndex()==0:
+            self.lbl_temperatura_actual.setText(str(datosCelsius.decode('utf-8')))#Este es el label que muestra la temperatura actual
+        elif self.comboBox_temperaturas.currentIndex()==1:
+            self.lbl_temperatura_actual.setText(str(datosFahrenheit.decode('utf-8')))#Este es el label que muestra la temperatura actual
+        else:
+            self.lbl_temperatura_actual.setText(str(datosHumedad.decode('utf-8')))#Este es el label que muestra la temperatura actual
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
